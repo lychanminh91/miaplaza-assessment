54a94217f95aa27a52cfbdbeff8d1ec9afb6a20b
Minh <mkgp3@MKGP3s-MacBook-Pro.local>
2024-08-06 16:20:40 +0700

    add changes

--- automation/obj/Debug/net8.0/automation.AssemblyInfo.cs
+++ automation/obj/Debug/net8.0/automation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("automation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71bd5320147928f8da8e8d756ca26a512e83eba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d81ebd38bb1cd7eec24e5acf6754d15e5e6025")]
 [assembly: System.Reflection.AssemblyProductAttribute("automation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("automation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
